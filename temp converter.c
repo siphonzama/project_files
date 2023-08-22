3700d7a0b75dd0c38ac477910aa63a6775b438c6
sipho <132463193+siphonzama@users.noreply.github.com>
2023-08-22 14:02:13 +0200

    first commit

---
+++ temp converter.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * a program that converts temperature from Celsius  to Fahrenheit */
+int main()
+{
+    int choice;
+
+    printf("Temperature Converter\n");
+    printf("1. Celsius to Fahrenheit\n");
+    printf("2. Fahrenheit to Celsius\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    int num;
+
+    printf("please enter a number that is temperature: ");
+    scanf("%d", &num);
+
+    int new_temp;
+
+     if (choice == 1) {
+         new_temp = ((num * 9/5) + 32);
+    } else if (choice == 2) {
+       new_temp = ((num - 32) * 5/9);
+    } else {
+        printf("Invalid choice.\n");
+    }
+
+    printf("your temperature is: %d\n", new_temp);
+
+    return 0;
+}
