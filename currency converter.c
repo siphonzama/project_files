9312294a6c58cb472e2383cf477fee4410be62c4
sipho <132463193+siphonzama@users.noreply.github.com>
2023-08-28 12:58:20 +0200

    Update currency converter.c

--- currency converter.c
+++ currency converter.c
@@ -1,44 +1,50 @@
 #include <stdio.h>
 
 /**A basic Currency Converter that will change U$D(Dollar) to ZAR(Rand)*/
+/**Do: starts a loop for the program*/
+/**while: sees if the program should be run again*/
 
 int main()
+{
+    char choice;
     /**Gets the information on which way to convert*/
-    int currency;
-
 do{
+    int currency;
+    int num;
+
     printf("Currency Converter\n");
-    printf("1. Rand to Dollar\n"):
-    printf("2. Dollar to Rand\n");
+    printf("1.Rand to Dollar\n");
+    printf("2.Dollar to Rand\n");
     printf("pick a way to convert: ");
     scanf("%d", &currency);
 
 /**Takes the number to be converted*/
-    int num;
+
     printf("Enter your amount: ");
     scanf("%d", &num);
-/**Convertion methods*/
+/**Conversion methods*/
     int new_num;
 
-    if (currency == 1){ 
-        new_num = (num*0,053);
+    if (currency == 1){
+        new_num = num * 0.053;
     }
     else if (currency == 2){
-        new_num = (num*18.70);
+        new_num = num * 18.70;
     }
-    else 
-        printf("not an otion");
-    
-}
-/**user interaction loop*/
+    else{
+        printf("not an option");
+        continue;
+        }
+    printf("Converted amount: %d\n", new_num);
 
-    char choice;
+/**sees if the choice runs again or ends program*/
 
-        printf("Do you want to continue? (Y/N): ")
+        printf("Do you want to continue? (Y/N): ");
         scanf(" %c", &choice);
 
-        while (choice == 'Y' || choice = 'y');
+} while (choice == 'Y' || choice == 'y');
 
-        printf("thank you for using the program:")
+    printf("thank you for using the program.\n");
 
-    Return 0
+    return 0;
+}
